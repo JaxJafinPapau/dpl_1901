910eb9a2280a73820d68c58281aa2e084359142c
jaxjafinpapau <jeremy.bennett8@gmail.com>
2019-02-13 09:37:02 -0700

    Updates book and test for expected year output

--- lib/book.rb
+++ lib/book.rb
@@ -3,11 +3,11 @@ class Book
               :author_last_name,
               :title,
               :publication_date
-              
+
   def initialize(book)
     @author_first_name = book[:author_first_name]
     @author_last_name = book[:author_last_name]
     @title = book[:title]
-    @publication_date = book[:publication_date]
+    @publication_date = book[:publication_date][-4..-1]
   end
 end
