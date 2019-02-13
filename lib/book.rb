2380a319227515fe14e2c1e99f81ce9642ada419
jaxjafinpapau <jeremy.bennett8@gmail.com>
2019-02-13 09:11:14 -0700

    Completes book and test

--- lib/book.rb
+++ lib/book.rb
@@ -1,5 +1,9 @@
 class Book
-
+  attr_reader :author_first_name,
+              :author_last_name,
+              :title,
+              :publication_date
+              
   def initialize(book)
     @author_first_name = book[:author_first_name]
     @author_last_name = book[:author_last_name]
