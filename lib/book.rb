ac7ffc46d9e55d0c00f1963999c25c56cc144df8
jaxjafinpapau <jeremy.bennett8@gmail.com>
2019-02-13 09:09:15 -0700

    Initial commit.

--- lib/book.rb
+++ lib/book.rb
@@ -1,4 +1,9 @@
 class Book
 
-
+  def initialize(book)
+    @author_first_name = book[:author_first_name]
+    @author_last_name = book[:author_last_name]
+    @title = book[:title]
+    @publication_date = book[:publication_date]
+  end
 end
--- test/book_test.rb
+++ test/book_test.rb
@@ -1,6 +1,17 @@
 require './test/test_helper'
 
 class BookTest < Minitest::Test
+  def setup
+    @book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+  end
 
+  def test_book_exists
+    assert_instance_of Book, @book
+  end
 
+  def test_book_has_attributes
+    assert_equal "Harper", @book.author_first_name
+    assert_equal "Lee", @book.author_last_name
+    assert_equal "To Kill a Mockingbird", @book.title
+  end
 end
