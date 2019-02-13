343ebf2c7bfd170d37de344244447ef25ac54f8d
jaxjafinpapau <jeremy.bennett8@gmail.com>
2019-02-13 09:49:15 -0700

    Completes author.add_book and test

--- lib/author.rb
+++ lib/author.rb
@@ -1,6 +1,22 @@
-class Author
+require './lib/book'
 
-  def initialize(argument)
-    @argument = argument
+class Author < Book
+  attr_reader :books,
+              :first_name,
+              :last_name
+
+  def initialize(author_info)
+    @books = []
+    @first_name = author_info[:first_name]
+    @last_name = author_info[:last_name]
+  end
+
+  def add_book(title, publication_date)
+    @books << Book.new({
+      author_first_name: @first_name,
+      author_last_name: @last_name,
+      title: title,
+      publication_date: publication_date
+      })
   end
 end
--- test/author_test.rb
+++ test/author_test.rb
@@ -4,8 +4,33 @@ class AuthorTest < Minitest::Test
 
   def setup
     @nk_jemisin = Author.new({first_name: "N.K.", last_name: "Jemisin"})
+    @the_fifth_season = Book.new({
+        author_first_name: "N.K.",
+        author_last_name: "Jemisin",
+        title: "The Fifth Season",
+        publication_date: "November 3, 2015"
+        })
+    @the_hundred_thousand_kingdoms = Book.new({
+      author_first_name: "N.K.",
+      author_last_name: "Jemisin",
+      title: "The Hundred Thousand Kingdoms",
+      publication_date: "2010"
+      })
   end
-  def test_author_exists
 
+  def test_author_exists
+    assert_instance_of Author, @nk_jemisin
+  end
+
+  def test_author_starts_with_no_books
+    assert_equal [], @nk_jemisin.books
+  end
+
+  def test_author_can_add_books
+    @nk_jemisin.add_book("The Fifth Season", "November 3, 2015")
+    assert_equal [@the_fifth_season], @nk_jemisin.books
+    @nk_jemisin.add_book("The Hundred Thousand Kingdoms", "2010")
+    assert_equal [@the_fifth_season, @the_hundred_thousand_kingdoms],
+                  @nk_jemisin.books
   end
 end
